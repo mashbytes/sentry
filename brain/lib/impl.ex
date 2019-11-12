9d749f301d19baa972cffc339fcb79d4478be09a
Matthew Hobbs <matthew.hobbs@yahoo.co.uk>
2019-11-12 06:58:32 +0000

    remove brain

--- brain/lib/impl.ex
+++
@@ -1,9 +0,0 @@
-defmodule Brain.Impl do
-
-    def on_nodes(state, nodes) do
-
-    end
-
-    def on_update(state, %Ears.State{} = ears) do
-    end
-end
--- brain/test/brain_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BrainTest do
-  use ExUnit.Case
-  doctest Brain
-
-  test "greets the world" do
-    assert Brain.hello() == :world
-  end
-end
